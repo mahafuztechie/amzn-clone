e5c36f26a630d043d1ed02a18ef213135328fe5e
mahafuztechie <mahafuzaadeez5@gmail.com>
2021-06-25 21:20:50 +0530

    permissions error solved

---
+++ permissions.js
@@ -0,0 +1,13 @@
+const permissions = 
+{
+  "type": "service_account",
+  "project_id": "clone-5aac1",
+  "private_key_id": process.env.PRIVATE_KEY_ID,
+  "private_key": process.env.PRIVATE_KEY,
+  "client_email": "firebase-adminsdk-treau@clone-5aac1.iam.gserviceaccount.com",
+  "client_id": "102138271255630671463",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-treau%40clone-5aac1.iam.gserviceaccount.com"
+}
